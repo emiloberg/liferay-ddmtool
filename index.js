76579a4621f38a923240cfc630ae0da1a87d3fe6
Emil Öberg <emil.oberg@monator.com>
2014-09-29 12:02:00 +0200

    moved all config and cache to $userhome/.ddmtool

--- index.js
+++ index.js
@@ -5,8 +5,6 @@
  *
  * TODO:
  *
- * Change so that cache are saved in separate folders for separate projects.
- *
  *
  *
  * Add tab complete for shell
@@ -53,13 +51,26 @@
  *
  */
 
+var argv							= require('minimist')(process.argv.slice(2));
 
 var Constants                       = require('./lib/Constants.js');
-var argv							= require('minimist')(process.argv.slice(2));
 var saveArgs	             		= require('./lib/routerSaveArgs.js');
 var router	             			= require('./lib/router.js');
-var LrClassNameConfig	    		= require('./lib/SingleLrClassNameConfig.js');
+var LrClassNameConfig	    		= require('./lib/ClassNameConfig.js');
 
+var utilities	                    = require('./lib/utilities.js');
+var Config							= require('./lib/Config.js');
+
+// Set paths
+var userHome 			= utilities.getUserHome();
+var ddmToolFolder 		= Constants.fetch('ddmToolFolder');
+var settingsFolder 		= Constants.fetch('settingsFolder');
+var projectsFolder 		= Constants.fetch('projectsFolder');
+var cacheFolder 		= Constants.fetch('cacheFolder');
+Config.set('userHome', userHome);
+Config.set('settingsFolder', userHome + '/' + ddmToolFolder + '/' + settingsFolder);
+Config.set('projectsFolder', userHome + '/' + ddmToolFolder + '/' + settingsFolder + '/' + projectsFolder);
+Config.set('cacheFolder', userHome + '/' + ddmToolFolder + '/' + cacheFolder);
 
 saveArgs(argv);
 LrClassNameConfig.loadCustomClassNames();
