bc0d5c1c65641f391a8cc667119fbe4af4e263f6
Emil Öberg <emil.oberg@monator.com>
2014-09-30 14:27:26 +0200

    added gruntfile for linting and releasing

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,26 @@ module.exports = function(grunt) {
 				node: true
 			},
 			jshintrc: '.jshintrc',
-		}
+		},
+		bump: {
+			options: {
+				files: ['package.json'],
+				updateConfigs: [],
+				commit: true,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ['package.json'],
+				createTag: true,
+				tagName: 'v%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: true,
+				pushTo: 'github',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+			}
+		},
 	});
 
+	grunt.loadNpmTasks('grunt-bump');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.registerTask('default', ['jshint']);
-
+	grunt.registerTask('release', ['bump']);
 };
